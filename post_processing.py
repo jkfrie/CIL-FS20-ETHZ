a2c892abb7318457c19a94147644fde8a0492c90
trapwired <dominic.weibel@protonmail.ch>
2020-07-23 17:02:30 +0200

    post_processing out-filed (to own file) and adjust fullyCNN_improved_datagenerator.ipynb

--- post_processing.py
+++ post_processing.py
@@ -1,8 +1,199 @@
-import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-from os import listdir
-from os.path import isfile, join
+import imageio
+from PIL import Image
+import cv2 as cv
+import natsort
+import os
+import matplotlib.pyplot as plt
+
+
+# Constants
+ROTATIONS = [0, 45, 90, 135, 180, 225, 270, 315]
+ROT_MARGIN = 89
+h, w = 608, 608
+
+
+# Rotate Image by angle degrees
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+    return result
+
+
+# helper function to plot images with subplots
+def add_to_subplot(img, axs, x, y, title):
+    axs[x, y].imshow(img)
+    axs[x, y].yaxis.set_visible(False)
+    axs[x, y].xaxis.set_visible(False)
+    axs[x, y].set_title(title)
+
+
+# print all eight prediction images
+def print_all_8(temp_im, name):
+    fig, axs = plt.subplots(3, 3, figsize=(20, 20))
+    for i in range(8):
+        add_to_subplot(temp_im[i], axs, i % 3, int(i / 3), name + ' ' + str(ROTATIONS[i]))
+
+
+# separate the rotated Images into 45 / 90 degree rotations
+def split_90_45(ar):
+    angle_90s = np.zeros((4, h, w), np.float)
+    angle_45s = np.zeros((4, h, w), np.float)
+    c45, c90 = 0, 0
+    for x in range(8):
+        if ROTATIONS[x] % 90 != 0:
+            angle_45s[c45] = ar[x]
+            c45 = c45 + 1
+        else:
+            angle_90s[c90] = ar[x]
+            c90 = c90 + 1
+    return angle_90s, angle_45s
+
+
+# split the image into 4 quadrants, tl = top-left / br = bottom-right
+def split_into_quadrants(ar):
+    x_len = len(ar)
+    y_len = len(ar[0])
+    tl = ar[0:int(x_len / 2), 0:int(y_len / 2)]
+    tr = ar[0:int(x_len / 2), int(y_len / 2): y_len]
+    bl = ar[int(x_len / 2):x_len, 0:int(y_len / 2)]
+    br = ar[int(x_len / 2):x_len, int(y_len / 2): y_len]
+    return tl, tr, bl, br
+
+
+# create one Image out of 4 quadrants, tl = top-left / br = bottom-right
+def combine_quadrants(tl, tr, bl, br):
+    top = np.column_stack((tl, tr))
+    bot = np.column_stack((bl, br))
+    tog = np.row_stack((top, bot))
+    return tog
+
+
+# combine the four quadrants of the 45 / 90 degree rotated predictions
+def comb_45_90(tl90, tr90, bl90, br90, tl45, tr45, bl45, br45):
+    tl = combine_it(tl90, tl45, 1)
+    tr = combine_it(tr90, tr45, 2)
+    bl = combine_it(bl90, bl45, 3)
+    br = combine_it(br90, br45, 4)
+    return combine_quadrants(tl, tr, bl, br)
+
+
+# helper method for comb_45_90()
+def combine_it(q90, q45, quadrant):
+    x_len = len(q90[0])
+    y_len = len(q90)
+    res = np.zeros((x_len, y_len), np.float)
+    if quadrant == 1:
+        # top left quadrant
+        for x in range(x_len):
+            for y in range(y_len):
+                if x + y < x_len:
+                    res[x][y] = q90[x][y]
+                else:
+                    res[x][y] = (q90[x][y] + q45[x][y]) / 2
+    elif quadrant == 2:
+        # top right quadrant
+        for x in range(x_len):
+            for y in range(y_len):
+                if x < y + 1:
+                    res[x][y] = q90[x][y]
+                else:
+                    res[x][y] = (q90[x][y] + q45[x][y]) / 2
+    elif quadrant == 3:
+        # bottom left quadrant
+        for x in range(x_len):
+            for y in range(y_len):
+                if x + 1 > y:
+                    res[x][y] = q90[x][y]
+                else:
+                    res[x][y] = (q90[x][y] + q45[x][y]) / 2
+    elif quadrant == 4:
+        # bottom right quadrant
+        for x in range(x_len):
+            for y in range(y_len):
+                if x + y >= x_len - 1:
+                    res[x][y] = q90[x][y]
+                else:
+                    res[x][y] = (q90[x][y] + q45[x][y]) / 2
+    return res
+
+
+# fuse the 8 predictions of the same Iamge together in different ways
+def eight_to_one(ar, mode):
+    res = ar[0]
+    a90, a45 = split_90_45(ar)
+    if mode == 'MAX':
+        max_ar = np.array(np.max(ar, axis=0), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    elif mode == 'MEAN':
+        mean = np.array(np.mean(ar, axis=0), np.float)
+        res = np.array((mean), dtype=np.uint8)
+    elif mode == 'SQUARE_MEAN':
+        square = np.array(np.square(ar), np.float)
+        mean = np.array(np.mean(square, axis=0), np.float)
+        res = thresholding(np.array((mean), dtype=np.uint8))
+    elif mode == 'only90_mean':
+        mean = np.array(np.mean(a90, axis=0), np.float)
+        res = thresholding_otsu(np.array((mean), dtype=np.uint8))
+    elif mode == 'only90_max':
+        max_ar = np.array(np.max(a90, axis=0), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    elif mode == 'only90_max_no_th':
+        max_ar = np.array(np.max(a90, axis=0), np.float)
+        res = np.array(max_ar, dtype=np.uint8)
+    elif mode == 'only90_max_tidy_contours':
+        max_ar = np.array(np.max(a90, axis=0), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+        res = tidy_contours(res, 500)
+    elif mode == 'median':
+        medi = np.array(np.median(ar, axis=0), np.float)
+        res = thresholding(np.array((medi), dtype=np.uint8))
+    elif mode == 'median_90s':
+        medi = np.array(np.median(a90, axis=0), np.float)
+        res = thresholding(np.array((medi), dtype=np.uint8))
+    elif mode == 'max_low_threshold':
+        max_ar = np.array(np.max(ar, axis=0), np.uint8)
+        ret3, res = cv.threshold(max_ar, 0, 255, cv.THRESH_BINARY)
+    elif mode == 'aggregate_and_count_10p':
+        f = lambda x: sum(i > 0.1 for i in x)
+        max_ar = np.array(np.apply_along_axis(f, 0, ar), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    elif mode == 'aggregate_and_count_50p':
+        f = lambda x: sum(i > 0.5 for i in x)
+        max_ar = np.array(np.apply_along_axis(f, 0, ar), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    elif mode == 'aggregate_and_count_tidy_contours':
+        f = lambda x: sum(i > 0.1 for i in x)
+        max_ar = np.array(np.apply_along_axis(f, 0, ar), np.float)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+        res = tidy_contours(res, 400)
+    elif mode == 'aggregate_and_count_45_90_dif_65':
+        # difference in 45 / 90 degree pics how to handle to prevent more info in middle
+        f = lambda x: sum(i > 0.65 for i in x)
+        sum_90 = np.array(np.apply_along_axis(f, 0, a90), np.float)
+        tl90, tr90, bl90, br90 = split_into_quadrants(sum_90)
+        sum_45 = np.array(np.apply_along_axis(f, 0, a45), np.float)
+        tl45, tr45, bl45, br45 = split_into_quadrants(sum_45)
+        max_ar = comb_45_90(tl90, tr90, bl90, br90, tl45, tr45, bl45, br45)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+        res = tidy_contours(res, 400)
+    elif mode == 'mean_45_90_dif':
+        mean_90 = np.array(np.mean(a90, axis=0), np.float)
+        tl90, tr90, bl90, br90 = split_into_quadrants(mean_90)
+        mean_45 = np.array(np.mean(a45, axis=0), np.float)
+        tl45, tr45, bl45, br45 = split_into_quadrants(mean_45)
+        max_ar = comb_45_90(tl90, tr90, bl90, br90, tl45, tr45, bl45, br45)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    elif mode == 'mean_45_90_dif_sq':
+        mean_90 = np.array(np.mean(a90, axis=0), np.float)
+        tl90, tr90, bl90, br90 = split_into_quadrants(mean_90)
+        mean_45 = np.array(np.mean(a45, axis=0), np.float)
+        tl45, tr45, bl45, br45 = split_into_quadrants(mean_45)
+        max_ar = comb_45_90(tl90, tr90, bl90, br90, tl45, tr45, bl45, br45)
+        max_ar = np.square(max_ar)
+        res = thresholding(np.array((max_ar), dtype=np.uint8))
+    return res
 
 
 # convert grayscale image to strict black and white Image
@@ -17,37 +208,10 @@ def thresholding(img):
     return th3
 
 
+# otsu - thresholding
 def thresholding_otsu(img):
     ret2, th2 = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     return th2
-
-
-# different modes of thresholding try out
-def thresholding_test(img, plot_images):
-    # global thresholding
-    ret1, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-    # Otsu's thresholding
-    ret2, th2 = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    # Otsu's thresholding after Gaussian filtering
-    blur = cv.GaussianBlur(img, (5, 5), 0)
-    ret3, th3 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    if plot_images:
-        # plot all the images and their histograms
-        images = [img, 0, th1,
-                  img, 0, th2,
-                  blur, 0, th3]
-        titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v=127)',
-                  'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
-                  'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
-        for i in range(3):
-            plt.subplot(3, 3, i * 3 + 1), plt.imshow(images[i * 3], 'gray')
-            plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
-            plt.subplot(3, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
-            plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
-            plt.subplot(3, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], 'gray')
-            plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
-        plt.show()
-    return th3
 
 
 # show Image, close on click? (t / f)
@@ -95,6 +259,7 @@ def tidy_contours(img, threshold):
     # show_image('after', thresh, True)
     return thresh
 
+
 # simple probabilistic (take only subset of points) hough-transform
 def hough_transform_probalistic():
     img = cv.imread('Results/Prediction_Images/test_10.png')
@@ -129,59 +294,80 @@ def hough_transform():
         cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
 
-# pipeline for post_processing im
-def post_process(im):
-    # post-processing Pipeline
-    gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-    ## hough?
-    # delete small blobs
-    res = tidy_contours(gray, 200)
-    return res
+# Load all test images, rotate them with ROTATIONS and store them in a list
+def prepare_test_images(n_test, test_image_dir, files_test, rotated_test_image_dir):
+    test_image_list = []
+    margin = ROT_MARGIN
+    for i in range(n_test):
+        orig = Image.open(test_image_dir + files_test[i])
+        # rotate each image 0, 45, 90, 135, 180, 225, 270, 315 degrees
+        for r in ROTATIONS:
+            width, height = orig.size
+            im2 = orig.rotate(r)
+            if r % 90 != 0:
+                # crop
+                w1, h1 = im2.size
+                im2 = im2.crop((margin, margin, w1 - margin, h1 - margin)).resize((width, height),
+                                                                                  resample=Image.BICUBIC)
+            name = files_test[i].split('.png')
+            im2.save(rotated_test_image_dir + name[0] + '_' + str(r) + '.png')
+        print('\tRotating ' + files_test[i])
+
+    new_test_im_dir = os.listdir(rotated_test_image_dir)
+    new_test_im_dir = natsort.natsorted(new_test_im_dir)
+    test_image_list = [imageio.imread(rotated_test_image_dir + file) for file in new_test_im_dir]
+    return test_image_list
 
 
-# entry point to post_process all pictures in dir_path and output them in output_path
-def post_process_dir(dir_path, output_path):
-    # get all png's in dir_path
-    only_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f)) and f.endswith(".png")]
-    # for all png's: apply post-processing and save in output_path
-    for file in only_files:
-        image = cv.imread(dir_path + file)
-        post_image = post_process(image)
-        cv.imwrite(output_path + '/post_' + file, post_image)
+# combine all 8 predicted Images with MODE and save to RESULT_DIR
+def combine_and_save(mode, result_dir, predictions, files_test):
+    # since test_images contains 8 Images per prediction test_image -> average over these 8 images
+    count = 0
+    dif_rot = len(ROTATIONS)
+    temp_im = np.zeros((8, h, w), np.float)  # hold all 8 pictures, then flatten along axis=0
+    new_predictions = np.zeros((94, h, w), dtype=np.uint8)
+    # start = random.randint(0,94) * 8
+    start = -1
+    for ti in predictions:
+        # read in image
+        img = cv.resize(ti, (608, 608))
+        ind = count % len(ROTATIONS)
 
+        if ROTATIONS[ind] % 90 != 0:
+            # 45 degree rotation
+            # first downsample
+            img = cv.resize(img, (430, 430), interpolation=cv.INTER_CUBIC)
+            # add margin
+            img = cv.copyMakeBorder(img, 89, 89, 89, 89, cv.BORDER_CONSTANT, value=[0, 0, 0])
+            # reverse rotation
+            img = rotate_image(img, -ROTATIONS[ind])
+            # resample to w,h
 
-# fit lines in all contours larger than threshold
-def find_lines(threshold):
-    # TODO process contours first: delete "arms"
-    img = cv.imread('Results/Prediction_Images/test_12.png')
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    thresh = thresholding(gray)
-    contours, hier = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    # cnt = contours[0]
+        else:
+            # its a 90 degree rotation
+            img = rotate_image(img, -ROTATIONS[ind])
 
-    # only fit contour if area over certain threshold, ie 1500
-    for c in contours:
-        mask = np.zeros(img.shape, dtype=np.uint8)
-        cv.fillPoly(mask, [c], [255, 255, 255])
-        mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-        pixels = cv.countNonZero(mask)
-        if pixels > threshold:
-            # then apply fitline() function
-            [vx, vy, x, y] = cv.fitLine(c, cv.DIST_HUBER, 0, 0.01, 0.01)
+        # last pic is never saved
+        if count % dif_rot == 0 and count != 0:
+            if count == start:
+                print_all_8(temp_im, files_test[int(count / len(ROTATIONS)) - 1])
+            # previous was the last pic
+            final_pred = eight_to_one(temp_im, mode)
+            new_predictions[int(count / 8)] = final_pred
+            out = Image.fromarray(final_pred, mode='L')
+            imageio.imwrite(result_dir + files_test[int(count / len(ROTATIONS)) - 1], out)
+            # reset, new pic
+            temp_im = np.zeros((8, h, w), np.float)
+        # build array with all 8 pics
+        temp_im[count % 8] = img
+        count = count + 1
 
-
-            # Now find two extreme points on the line to draw line
-            lefty = int((-x * vy / vx) + y)
-            righty = int(((gray.shape[1] - x) * vy / vx) + y)
-
-            # Finally draw the line in pink
-            cv.line(img, (gray.shape[1] - 1, righty), (0, lefty), (127, 0, 255), 1)
-    cv.imshow('img', img)
-    # cv.imwrite('test.png', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    # last pic save
+    final_pred = eight_to_one(temp_im, mode)
+    new_predictions[count % dif_rot] = final_pred
+    out = Image.fromarray(final_pred, mode='L')
+    imageio.imwrite(result_dir + files_test[int(count / len(ROTATIONS)) - 1], out)
 
 
 if __name__ == '__main__':
-    find_lines(1500)
-    # post_process_dir("Results/Prediction_Images/", 'Results/Post_Processed')
+    print('hi')
